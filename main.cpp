25a1fc0e4df46b686ac1dd57406d809417c08d55
Burkay Tunçtürk <burkay.tuncturk@gmail.com>
2024-11-13 20:53:31 +0300

    Array generator added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+using namespace std;
 
+void generateSortedRandomArray(int* array, int size, int maxValue) {
+    srand(std::time(nullptr));
+
+    for (int i = 0; i < size; ++i) {
+        array[i] = rand() % (maxValue+ 1);
+    }
+
+    sort(array, array + size);
+}
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int maxValue = 100;
+    int size = 100;
+    int* array = new int[size];
+
+    generateSortedRandomArray(array, size, maxValue);
+    for (int i = 0; i < size; ++i) {
+        cout << array[i] << endl;
+    }
+
+    delete[] array;
     return 0;
 }
